88adfa626f291cd57a72dc9011e55aa254b1f8d8
Alex Falke <afalkear@gmail.com>
2013-02-08 11:28:41 -0300

    [#8370445] MailModel generated

---
+++ spec/models/mail_model_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe MailModel do
+
+  it { should validate_presence_of :subject }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :account_id }
+
+end
