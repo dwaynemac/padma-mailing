f443750e3549314d4c2216414859af57a7a995e6
Alex Falke <afalkear@gmail.com>
2013-02-15 11:18:58 -0300

    [#8370445] moved webservices keys to ENV variable

--- config/initializers/padma_webservices.rb
+++ config/initializers/padma_webservices.rb
@@ -7,7 +7,7 @@ end
 
 module ActivityStream
   HYDRA = ::HYDRA
-  API_KEY = "6d1a2dd931ef48d5f0c4d62de773825d3369ab426811c79c55e40569bc7bf044a437bbf569f765e6fd3a282ab43a27a2cb48ee2bd08c8bf743190165cd2ecb76"
+  API_KEY = ENV['activities_key']
   LOCAL_APP_NAME = 'crm'
 end
 
