4957389fd7b74ee6f99678df3dc4d5471c7aa4d0
Alex Falke <afalkear@gmail.com>
2013-02-17 20:44:21 -0300

    [#44086169] created mail_model in PadmaMailer

--- app/mailers/padma_mailer.rb
+++ app/mailers/padma_mailer.rb
@@ -1,8 +1,18 @@
 class PadmaMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def single_mail(email, subject, content)
-    @greeting = content
-    mail to: email, subject: subject
+  def mail_model(school, model, recipient, bcc=nil, from=nil)
+    return if model.nil?
+    return if recipient.blank?
+
+    @recipients = recipient
+    @bcc = bcc
+    @from = (from.blank?)? "'#{model.school.name}' <#{model.school.email}>" : from
+    @subject = model.subject
+    @sent_on = Time.zone.now
+    @content_type = 'multipart/mixed'
+    part :content_type => "text/html", :body => render_message( 'mail_model',
+                                                                :content => model.content,
+                                                                :school => school )
   end
 end
