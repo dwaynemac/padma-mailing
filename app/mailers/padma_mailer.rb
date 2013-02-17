8744b11931464598b92a754c3e10b11da06334d7
Alex Falke <afalkear@gmail.com>
2013-02-17 20:54:39 -0300

    [#44086169] added mail configurations and mail model

--- app/mailers/padma_mailer.rb
+++ app/mailers/padma_mailer.rb
@@ -1,18 +1,17 @@
 class PadmaMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def mail_model(school, model, recipient, bcc=nil, from=nil)
+  def mail_model(model, recipient, bcc=nil, from=nil)
     return if model.nil?
     return if recipient.blank?
 
     @recipients = recipient
     @bcc = bcc
-    @from = (from.blank?)? "'#{model.school.name}' <#{model.school.email}>" : from
+    @from = (from.blank?)? "'#{model.account.name}' " : from
     @subject = model.subject
     @sent_on = Time.zone.now
     @content_type = 'multipart/mixed'
     part :content_type => "text/html", :body => render_message( 'mail_model',
-                                                                :content => model.content,
-                                                                :school => school )
+                                                                :content => model.content)
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,22 @@ Mailing::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # Email options
+  Mailit::Application.configure do
+    config.action_mailer.default_url_options = { host: "localhost:3000" }
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        domain: "afalke.com.ar",
+        authentication: "plain",
+        enable_starttls_auto: true,
+        user_name: ENV["GMAIL_USERNAME"],
+        password: ENV["GMAIL_PASSWORD"]
+    }
+    # Rest of file omitted.
+  end
 
   config.before_configuration do
     env_file = File.join(Rails.root, 'config', 'local_env.yml')
