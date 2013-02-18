d22cfe2512b878612fbec7929ada2cd7b9c73749
Alex Falke <afalkear@gmail.com>
2013-02-18 00:07:27 -0300

    [#44086169] specs to test mail delivery

--- spec/mailers/padma_mailer_spec.rb
+++ spec/mailers/padma_mailer_spec.rb
@@ -1,5 +1,16 @@
 require "spec_helper"
 
 describe PadmaMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be able to send a template mail" do
+    @subject = "Hola Alex"
+    recipient = "afalkear@gmail.com"
+    account = FactoryGirl.create(:account)
+    template = Template.new(name: "new_template", subject: @subject, content: "this will be the content")
+    template.account = account
+    template.save!
+
+    PadmaMailer.mail_template(template, recipient).deliver
+    last_email.to.should include(recipient)
+    last_email.subject.should eq(@subject)
+  end
 end
