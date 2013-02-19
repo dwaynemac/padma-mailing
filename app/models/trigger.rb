5b39c898d9e23a93de82647501529a8608362538
Dwayne Macgowan <dwaynemac@gmail.com>
2013-02-19 18:39:37 -0300

    [#8370465] fix relation

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -8,7 +8,7 @@ class Trigger < ActiveRecord::Base
   validates_presence_of :local_account_id
   belongs_to :account, :class_name => "Account", :foreign_key => :local_account_id
 
-  has_many :templates_triggerses, dependent: :destroy
+  has_many :templates_triggerses, dependent: :destroy, class_name: 'TemplatesTriggers'
   has_many :templates, through: :templates_triggerses
 
   has_many :filters, dependent: :destroy
