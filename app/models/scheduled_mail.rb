0d5a381ee2e06ac912e55e6e2df5d0be7aa698b8
Alex Falke <afalkear@gmail.com>
2013-03-03 18:19:38 -0300

    [#44086553] moving deliver method to ScheduledMail

--- app/models/scheduled_mail.rb
+++ app/models/scheduled_mail.rb
@@ -8,7 +8,34 @@ class ScheduledMail < ActiveRecord::Base
 
   def deliver_now!
     return unless delivered_at.nil?
+
     PadmaMailer.template(template, recipient_email, nil, account.padma.email).deliver
     update_attribute :delivered_at, Time.now
+
+
+  end
+
+  def creation_activity(contact_id, user)
+    ActivityStream::Activity.new(target_id: contact_id, target_type: 'Contact',
+                                 object_id: self.id, object_type: 'Template',
+                                 generator: ActivityStream::LOCAL_APP_NAME,
+                                 content: "Mail sent: #{self.name}",
+                                 public: false,
+                                 username: user.username,
+                                 account_name: user.current_account.name,
+                                 created_at: Time.zone.now.to_s,
+                                 updated_at: Time.zone.now.to_s )
+  end
+
+  def deliver(to, bcc, from, user, contact_id)
+
+    # Deliver mail
+    self.deliver_now!
+
+    # Send notification to activities
+    if !contact_id.nil?
+      a = creation_activity(contact_id, user)
+      a.create(username: user.username, account_name: user.current_account.name)
+    end
   end
 end
