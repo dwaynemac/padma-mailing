fbe1dd7e9e01df8a6b9c06cb4fdee83126b3a2a8
Alex Falke <afalkear@gmail.com>
2013-03-05 12:00:11 -0300

    [#44086553] scheduledMail delivers and notifies activities

--- app/models/template.rb
+++ app/models/template.rb
@@ -11,23 +11,15 @@ class Template < ActiveRecord::Base
   has_many :templates_triggerses, dependent: :destroy
   has_many :triggers, through: :templates_triggerses
 
-  def creation_activity(contact_id, user)
-    ActivityStream::Activity.new(target_id: contact_id, target_type: 'Contact',
-                                 object_id: self.id, object_type: 'Template',
-                                 generator: ActivityStream::LOCAL_APP_NAME,
-                                 content: "Mail sent: #{self.name}",
-                                 public: false,
-                                 username: user.username,
-                                 account_name: user.current_account.name,
-                                 created_at: Time.zone.now.to_s,
-                                 updated_at: Time.zone.now.to_s )
-  end
 
-  def schedule_deliver(to, bcc, from, user, contact_id)
+  def deliver(data)
+    user = data[:user]
     schedule = ScheduledMail.create(
                                  template_id: this,
                                  local_account_id: user.current_account.id,
-                                 recipient_email: to)
-    schedule.deliver(to, bcc, from, user, contact_id)
+                                 recipient_email: data[:to],
+                                 contact_id: data[:contact_id],
+                                 username: user.username)
+    schedule.deliver_now!
   end
 end
