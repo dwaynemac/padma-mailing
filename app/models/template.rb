d4d39eb83bff0f692c5a7c6fa98a9ba86330aa3f
Dwayne Macgowan <dwaynemac@gmail.com>
2013-03-09 20:15:22 -0300

    [#45692535] fix destroy button

--- app/models/template.rb
+++ app/models/template.rb
@@ -8,8 +8,8 @@ class Template < ActiveRecord::Base
   validates_presence_of :account
   belongs_to :account, class_name: "Account", foreign_key: :local_account_id
 
-  has_many :templates_triggerses, dependent: :destroy
-  has_many :triggers, through: :templates_triggerses
+  has_many :templates_triggerses, dependent: :destroy, class_name: 'TemplatesTriggers'
+  has_many :triggers, through: :templates_triggerses, class_name: 'TemplatesTriggers'
 
 
   def deliver(data)
