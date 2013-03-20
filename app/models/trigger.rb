68d6878d38f4bb575d6aeec5d153a10df733f31a
Dwayne Macgowan <dwaynemac@gmail.com>
2013-03-20 16:48:33 -0300

    [#46554375] username was missing too

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -40,6 +40,7 @@ class Trigger < ActiveRecord::Base
                 local_account_id: tt.template.local_account_id,
                 recipient_email: recipient_email,
                 contact_id: data['contact_id'],
+                username: data['username'],
                 send_at: send_at
             )
           end
