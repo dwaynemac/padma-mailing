370ac5a890d99ae07098464017051e4fda5f2fe9
Dwayne Macgowan <dwaynemac@gmail.com>
2013-03-20 13:33:13 -0300

    show latest messages first

--- app/controllers/scheduled_mails_controller.rb
+++ app/controllers/scheduled_mails_controller.rb
@@ -3,7 +3,7 @@ class ScheduledMailsController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @scheduled_mails = @scheduled_mails.includes(:template).order('send_at asc')
+    @scheduled_mails = @scheduled_mails.includes(:template).order('send_at desc')
   end
 
   def destroy
