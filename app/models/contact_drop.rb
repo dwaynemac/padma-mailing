eada19ce4c831acb14fec30f5bd9ec7e23a28a3b
Luis Perichon <luisperichon@gmail.com>
2014-05-26 00:31:13 -0300

    [#44086353]use alias_method to support kshêma variables

--- app/models/contact_drop.rb
+++ app/models/contact_drop.rb
@@ -1,19 +1,39 @@
 class ContactDrop < Liquid::Drop
 
   def initialize(contact)
-    puts "initialize"
     @contact = contact
   end
 
-  def nombre_completo
+  def full_name
     "#{@contact.first_name} #{@contact.last_name}"
   end
+  alias_method :nombre_completo, :full_name
 
-  def nombres
+  def first_name
     @contact.first_name
   end
+  alias_method :nombres, :first_name
 
-  def apellidos
+  def last_name
     @contact.last_name
   end
+  alias_method :apellidos, :last_name
+
+  def gender
+    @contact.gender.to_sym
+  end
+  alias_method :genero, :gender
+
+  def a_u_o
+    gender == :female ? 'a' : 'o'
+  end
+
+  #horario
+    #horario.nombre, horario.hora, horario.instructor
+
+  #instructor
+    #instructor.nombre, instructor.genero, instructor.firma, instructor.mail, instructor.a_u_o
+
+  #plan_vigente
+    #plan_vigente.nombre, plan_vigente.fecha_inicio, plan_vigente.fecha_fin
 end
