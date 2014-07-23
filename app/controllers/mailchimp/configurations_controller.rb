ba4c9d069aa2884f8015f0be563e4e61edb3c520
sebareca <sebareca@gmail.com>
2014-07-23 20:00:27 -0300

    [#75113332] Move Mailchimp -> Mailchimp::Configuration (Controller)

--- app/controllers/mailchimp/configurations_controller.rb
+++ app/controllers/mailchimp/configurations_controller.rb
@@ -1,2 +1,54 @@
 class Mailchimp::ConfigurationsController < ApplicationController
+  before_filter :get_configuration
+  before_filter :require_set_up, only: [:show]
+  
+  def show
+    render text: "Mailchimp show"
+  end
+
+  def new
+    @configuration = Mailchimp::Configuration.new
+  end
+
+  def create
+    @configuration = Mailchimp::Configuration.create(
+      local_account_id: current_user.current_account.id,
+      api_key: params[:configuration][:api_key]
+    )
+    redirect_to primary_list_mailchimp_configuration_path @configuration
+  end
+
+  def primary_list
+    @lists = @configuration.lists
+  end
+  
+  def update
+    @configuration.update_attributes(configuration_params)
+    redirect_to mailchimp_configuration_path
+  end
+
+  def destroy
+    @configuration.destroy
+    redirect_to mailchimp_configuration_path
+  end
+
+  private
+  
+  def get_configuration
+    @configuration = current_user.current_account.mailchimp_configuration
+  end
+
+  def require_set_up
+    if @configuration.nil?
+      redirect_to new_mailchimp_configuration_path
+    elsif @configuration.primary_list.nil?
+      redirect_to primary_list_mailchimp_configuration_path
+    elsif @configuration.primary_list.segments.empty?
+      redirect_to segments_lists_path
+    end
+  end
+  
+  def configuration_params
+    params.require(:configuration).permit(:primary_list_id)
+  end
 end
--- app/models/mailchimp/configuration.rb
+++ app/models/mailchimp/configuration.rb
@@ -1,10 +1,16 @@
 class Mailchimp::Configuration < ActiveRecord::Base
+  
+  # This is a bug in Rails 3.2 that is fixed in Rails 4.0.0.
+  self.table_name = 'mailchimp_configurations'
+  
   attr_accessible :api_key
   attr_accessible :local_account_id
   attr_accessible :primary_list_id
   
   belongs_to :account, foreign_key: :local_account_id
   has_many :lists
+  
+  after_create :sync_lists
   
   # @return [Gibbon:API]
   def api
@@ -17,6 +23,20 @@ class Mailchimp::Configuration < ActiveRecord::Base
 
   def primary_list
     List.find(primary_list_id) 
+  end
+  
+  private
+  
+  def sync_lists
+    api.lists.list['data'].each do |list_hash|
+      if List.where(api_id: list_hash['id']).empty?    
+        List.create(
+          api_id: list_hash['id'],
+          name: list_hash['name'],
+          mailchimp_configuration_id: id
+        )
+      end
+    end
   end
 
 end
