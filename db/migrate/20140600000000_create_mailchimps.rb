5d874cf11ef2a1c6b09a14c11dae8550516e3764
sebareca <sebareca@gmail.com>
2014-07-28 17:42:48 -0300

    [75113332] Fix inconsistencies in db

---
+++ db/migrate/20140600000000_create_mailchimps.rb
@@ -0,0 +1,14 @@
+class CreateMailchimps < ActiveRecord::Migration
+  def change
+    create_table :mailchimps do |t|
+      t.string :list
+    end
+    create_table :segments do |t|
+      t.integer :mailchimp_id
+    end
+    create_table :mailchimp_integrations do |t|
+    end 
+    create_table :lists do |t|
+    end 
+  end
+end
