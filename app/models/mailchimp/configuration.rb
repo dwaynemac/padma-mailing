b7fd0fa5ba5b3a1fc8b4ce2ec76d8f11a653a6fe
sebareca <sebareca@gmail.com>
2014-08-10 13:25:51 -0300

    [#75113332] Configuration new methods
    1) Create synchronizer (in contacts) before create
    
    2) Update synchronizer

--- app/models/mailchimp/configuration.rb
+++ app/models/mailchimp/configuration.rb
@@ -1,3 +1,5 @@
+require 'rest_client'
+
 class Mailchimp::Configuration < ActiveRecord::Base
   
   # This is a bug in Rails 3.2 that is fixed in Rails 4.0.0.
@@ -6,12 +8,13 @@ class Mailchimp::Configuration < ActiveRecord::Base
   attr_accessible :api_key
   attr_accessible :local_account_id
   attr_accessible :primary_list_id
+  attr_accessible :synchronizer_id
   
   belongs_to :account, foreign_key: :local_account_id
   has_many :mailchimp_lists, foreign_key: :mailchimp_configuration_id, class_name: "Mailchimp::List"
   
-  after_create :create_lists_in_mailchimp
-  after_create :create_contacts_synchronizer
+  before_create :create_synchronizer
+  after_create :create_mailchimp_lists_locally
   
   # @return [Gibbon:API]
   def api
@@ -22,28 +25,33 @@ class Mailchimp::Configuration < ActiveRecord::Base
     @mailchimp_api
   end
 
-  def primary_list
-    Mailchimp::List.find(primary_list_id) 
-  end
-
-  def create_contacts_synchronizer
-    debugger
-    list = primary_list
-
+  def create_synchronizer
     response = RestClient.post Contacts::HOST + '/v0/mailchimp_synchronizers',
+      app_key: Contacts::API_KEY,
+      account_name: account.name,
+      synchronizer: {api_key: api_key}
+    
+    self.synchronizer_id = JSON.parse(response)['id']   
+  end
+  
+  def primary_list
+    Mailchimp::List.find(primary_list_id)
+  end
+  
+  def update_synchronizer
+    response = RestClient.put Contacts::HOST + '/v0/mailchimp_synchronizers/' + self.synchronizer_id.to_s,
+      id: synchronizer_id,
       app_key: Contacts::API_KEY,
       account_name: account.name,
       synchronizer: {
         api_key: api_key,
-        list_id: primary_list_id,
+        list_id: primary_list.api_id
       }
-    
-    puts response
   end
   
   private
   
-  def create_lists_in_mailchimp
+  def create_mailchimp_lists_locally
     api.lists.list['data'].each do |list_hash|
       if Mailchimp::List.where(api_id: list_hash['id']).empty?    
         Mailchimp::List.create(
