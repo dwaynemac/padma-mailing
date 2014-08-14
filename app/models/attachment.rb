f14dad41de63ba213efc9bcd10975506eb221d8e
Alex Falke <afalkear@gmail.com>
2014-08-14 17:26:52 -0300

    [#71883734] do not validate attachment as it can be any file type

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,4 +2,6 @@ class Attachment < ActiveRecord::Base
   attr_accessible :attachment_content_type, :attachment_file_name, :attachment_file_size, :attachment_updated_at, :template_id, :attachment
   belongs_to :template
   has_attached_file :attachment
+
+  do_not_validate_attachment_file_type :csv_file
 end
