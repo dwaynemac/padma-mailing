595acebffef7489f5782b9389cc970716d17eb41
Dwayne Macgowan <dwaynemac@gmail.com>
2015-01-07 11:29:30 -0300

    [#75113332] simplify and improve dinamic form using nested_form gem

--- app/assets/javascripts/mailchimp/lists.js.coffee
+++ app/assets/javascripts/mailchimp/lists.js.coffee
@@ -4,10 +4,3 @@
 
 $(document).ready ->
   $('[data-toggle="tooltip"]').tooltip()
-
-@addSegment = ->
-  new_fields = $('#templateForm .mailchimp_segment_fields:first').clone()
-  $('.mailchimp_segment_fields:last').after(new_fields)
-
-@removeSegment = (e) ->
-  $($(e).parents('.mailchimp_segment_fields')[0]).remove()
--- app/controllers/mailchimp/lists_controller.rb
+++ app/controllers/mailchimp/lists_controller.rb
@@ -5,10 +5,15 @@ class Mailchimp::ListsController < ApplicationController
 
   def update
     @list = Mailchimp::List.find(params[:id])
-    @list.update_attributes(params[:mailchimp_list])
-    @list.mailchimp_configuration.update_attribute(:filter_method ,params[:filter_method])
-    @list.mailchimp_configuration.update_synchronizer
-    
-    redirect_to @list.mailchimp_configuration
+    if @list.update_attributes(params[:mailchimp_list])
+      @list.mailchimp_configuration
+           .update_attribute(:filter_method ,params[:filter_method]) # no validation here.
+      @list.mailchimp_configuration.update_synchronizer
+
+      redirect_to @list.mailchimp_configuration
+    else
+      flash.alert = t('mailchimp.list.update.couldnt_update_segments')
+      render 'segments'
+    end
   end
 end
