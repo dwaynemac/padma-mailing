086b7a60a9b696db154b63fbdebc2af469ce02b5
Dwayne Macgowan <dwaynemac@gmail.com>
2016-04-03 14:03:28 -0300

    remove unused file

--- app/controllers/mailchimp/synchronizers_controller.rb
+++
@@ -1,8 +0,0 @@
-class Mailchimp::SynchronizersController < Mailchimp::PetalController
-
-  def sync_now
-    @configuration = current_user.current_account.mailchimp_configuration
-    Mailchimp::Configuration.
-
-  end
-end
