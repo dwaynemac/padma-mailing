069867d82d16f4b8a1475ac1691fc0297005499d
Dwayne Macgowan <dwaynemac@gmail.com>
2016-05-28 17:59:51 -0300

    spec showing bug

---
+++ spec/helpers/mailchimp/subscriptions_helper_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe Mailchimp::SubscriptionsHelper do
+  describe "mailchimp_enabled?" do
+    subject { mailchimp_enabled? }
+    describe "if account has no enabled_petals ([])" do
+      let(:enabled_petals){[]}
+      before { stub_current_user }
+      it { should be_false }
+    end
+    describe "if account has enabled_petals nil" do
+      let(:enabled_petals){nil}
+      before { stub_current_user }
+      it { should be_false }
+    end
+    describe "if account has other enabled petals, not mailchimp" do
+      let(:enabled_petals){%W(petal1 petal2)}
+      before { stub_current_user }
+      it { should be_false }
+    end
+    describe "if account has mailchimp in its enabled petals" do
+      let(:enabled_petals){%W(petal1 petal2 mailchimp)}
+      before { stub_current_user }
+      it { should be_true }
+    end
+  end
+
+  def stub_current_user
+    @account = FactoryGirl.create(:account)
+    pa = PadmaAccount.new(name: @account.name, enabled: true, enabled_petals: enabled_petals)
+    Account.any_instance.stub(:padma).and_return(pa)
+
+    @user = FactoryGirl.create(:user)
+    pu = PadmaUser.new(username: @user.username)
+
+    @user.stub(:current_account).and_return(@account)
+
+    User.any_instance.stub(:padma_enabled?).and_return true
+    User.any_instance.stub(:padma).and_return pu
+    
+    RSpec::Core::ExampleGroup::Nested_1::Nested_1::Nested_1.any_instance.stub(:current_user).and_return(@user)
+    RSpec::Core::ExampleGroup::Nested_1::Nested_1::Nested_2.any_instance.stub(:current_user).and_return(@user)
+    RSpec::Core::ExampleGroup::Nested_1::Nested_1::Nested_3.any_instance.stub(:current_user).and_return(@user)
+    RSpec::Core::ExampleGroup::Nested_1::Nested_1::Nested_4.any_instance.stub(:current_user).and_return(@user)
+    helper.stub(:current_user).and_return(@user)
+  end
+end
