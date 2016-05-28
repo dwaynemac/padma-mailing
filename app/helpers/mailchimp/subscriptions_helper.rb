46da7932ab046e0019ab845c42f7c4fabe3109f1
Dwayne Macgowan <dwaynemac@gmail.com>
2016-05-28 18:01:09 -0300

    fixed bug

--- app/helpers/mailchimp/subscriptions_helper.rb
+++ app/helpers/mailchimp/subscriptions_helper.rb
@@ -1,7 +1,7 @@
 module Mailchimp::SubscriptionsHelper
 
   def mailchimp_enabled?
-    'mailchimp'.in?(current_user.current_account.padma.enabled_petals)
+    'mailchimp'.in?(current_user.current_account.padma.enabled_petals||[])
   end
 
 end
