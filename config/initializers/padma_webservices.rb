b019f88f94fe7ebe5658319ab85441c5a477f9aa
Dwayne Macgowan <dwaynemac@gmail.com>
2016-10-07 15:52:30 +0000

    consider cloud9 for development environment

--- config/initializers/padma_webservices.rb
+++ config/initializers/padma_webservices.rb
@@ -6,7 +6,7 @@ CRM_HOST =case Rails.env
   when "staging"
     "padma-crm-staging.herokuapp.com"
   when "development"
-    "localhost:3000"
+    APP_CONFIG['crm-url'].gsub("https://",'')
   when "test"
     "localhost:3000"
 end
@@ -14,13 +14,20 @@ end
 module Accounts
   HYDRA = ::HYDRA
   API_KEY = ENV['accounts_key']
+  if APP_CONFIG['on-cloud9']
+    HOST = APP_CONFIG['accounts-url'].gsub("http://",'')
+  end
 end
 
 module Contacts
   HYDRA = ::HYDRA
   API_KEY = ENV['contacts_key']
   if Rails.env.development?
-    HOST = "localhost:3002"
+    if APP_CONFIG['on-cloud9']
+      HOST = APP_CONFIG['contacts-url'].gsub("http://",'')
+    else
+      HOST = "localhost:3002"
+    end
   end
 end
 
@@ -28,6 +35,9 @@ module ActivityStream
   HYDRA = ::HYDRA
   API_KEY = ENV['activities_key']
   LOCAL_APP_NAME = 'mailing'
+  if APP_CONFIG['on-cloud9']
+    HOST = APP_CONFIG['activity-stream-url'].gsub("https://",'')
+  end
 end
 
 class LogicalModel
