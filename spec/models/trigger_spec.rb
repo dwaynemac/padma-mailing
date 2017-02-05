1d8c44d90a8272b6745bc4c11b86e048c306ffe0
Dwayne Macgowan <dwaynemac@gmail.com>
2017-02-05 16:16:12 +0000

    try to avoid erratic spec failure

--- spec/models/trigger_spec.rb
+++ spec/models/trigger_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Trigger do
+  before do
+    RSpec::Mocks.proxy_for(PadmaAccount).reset
+  end
   it { should belong_to(:account).with_foreign_key(:local_account_id) }
   it { should validate_presence_of(:local_account_id) }
   it { should have_many :filters }
