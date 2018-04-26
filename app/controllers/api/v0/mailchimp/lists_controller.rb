966f299787b08ae83f3a1c7b5cc882e566323928
Alex Falke <afalkear@gmail.com>
2018-04-26 18:15:40 -0300

    minor change

--- app/controllers/api/v0/mailchimp/lists_controller.rb
+++ app/controllers/api/v0/mailchimp/lists_controller.rb
@@ -5,8 +5,6 @@ class Api::V0::Mailchimp::ListsController < Api::V0::ApiController
   list = Mailchimp::List.find(params[:id])
   list.create_activity(params)
   
-  respond_to do |format|
-    format.json { render json: nil, status: :ok }
-  end
+  render json: nil, status: :ok 
  end
 end
