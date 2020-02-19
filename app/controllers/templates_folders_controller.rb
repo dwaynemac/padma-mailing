5a87f6d6e009ac48c342ac4a68ebc1a3abf86baa
Dwayne Macgowan <dwaynemac@gmail.com>
2020-02-19 18:03:43 -0300

    update folder name

--- app/controllers/templates_folders_controller.rb
+++ app/controllers/templates_folders_controller.rb
@@ -11,6 +11,16 @@ class TemplatesFoldersController < ApplicationController
     end
   end
 
+  def update
+    @templates_folder.update_attributes(params[:templates_folder])
+    respond_to do |format|
+      format.html do
+        redirect_to templates_path(folder_id: @templates_folder.parent_templates_folder_id), notice: "ok!"
+      end
+      format.json { render json: { templates_folder: @templates_folder.attributes } } 
+    end
+  end
+  
   def destroy
     @templates_folder.destroy
     respond_to do |format|
