4381373c03280c7100543e10cf73ffe3eb999467
Dwayne Macgowan <dwaynemac@gmail.com>
2020-02-20 14:37:02 -0300

    breadcrums

--- app/helpers/templates_folders_helper.rb
+++ app/helpers/templates_folders_helper.rb
@@ -3,4 +3,32 @@ module TemplatesFoldersHelper
   def folder_icon
     "<span class='glyphicon glyphicon-folder-open'></span>".html_safe
   end
+
+  def breadcrums(folder=nil)
+    folder = @current_folder if folder.nil?
+    ret = ""
+    if folder
+      if folder.parent
+        ret << breadcrums(folder.parent) 
+        ret << " / "
+      end
+      ret << rip_name(folder).html_safe
+    end
+  end
+
+  def link_to_folder(folder)
+    link_to folder.name, templates_path(folder_id: folder.id)
+  end
+
+  def rip_name(folder)
+    content_tag :span,
+      class: "rest_in_place",
+      data: {
+        url: url_for(folder),
+        object: "templates_folder",
+        attribute: "name"
+      } do
+      folder.name
+    end
+  end
 end
