870713e9d0f89f30e971df1c53dce206c3fddbb1
Alex Falke <afalkear@gmail.com>
2020-10-27 17:37:57 -0300

    update config files

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mailing::Application.config.secret_token = 'dbcc9d510dde3befe33f1f36ec580b8d1d765d9166c8645e50131bfe2cd0099b4b29b9a6d5b464debc736037427f0dc9ee433b9d5e58d21a15fc2afc183cdf24'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mailing::Application.config.secret_key_base = 'e900eb6a443ff310c3741651d20d5b391f77713a9236c236e9ac78b95ee4bab8157a9935eba93bdafc96cb3b8011d596cd1d482dc7c036444eb7727eaf4e0b9e'
