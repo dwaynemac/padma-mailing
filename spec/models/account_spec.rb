978187be7dc14bd503c780a0da30c0971a2c0c4f
Alex Falke <afalkear@gmail.com>
2020-11-01 13:28:04 -0300

    update this spec

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -10,7 +10,7 @@ describe Account do
   it {should validate_presence_of :name}
   it {should validate_uniqueness_of :name}
 
-  it { should have_many(:templates).with_foreign_key(:local_account_id)}
+  it { should have_many(:templates).class_name('Template').with_foreign_key(:local_account_id)}
 
   describe "#padma" do
     it "should give access to padma accounts api" do
