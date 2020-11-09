fb83fc5ae74af9a14c9c2d7dfef4b153c25d30a8
Alex Falke <afalkear@gmail.com>
2020-11-09 20:59:20 -0300

    update rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,10 +8,6 @@ require 'spec_helper'
 
 Coveralls.wear!
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
