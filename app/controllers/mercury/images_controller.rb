53c26b8a1e95bed7f685d98ec2db419112e11272
Alex Falke <afalkear@gmail.com>
2020-11-15 18:02:03 -0300

    comment out mercury code

--- app/controllers/mercury/images_controller.rb
+++ app/controllers/mercury/images_controller.rb
@@ -1,19 +1,19 @@
-class Mercury::ImagesController < MercuryController
+#class Mercury::ImagesController < MercuryController
 
-  respond_to :json
+#  respond_to :json
 
   # POST /images.json
-  def create
-    @image = Mercury::Image.new(params[:image])
-    @image.save
-    respond_with @image
-  end
+#  def create
+#    @image = Mercury::Image.new(params[:image])
+#    @image.save
+#    respond_with @image
+#  end
 
   # DELETE /images/1.json
-  def destroy
-    @image = Mercury::Image.find(params[:id])
-    @image.destroy
-    respond_with @image
-  end
+#  def destroy
+#    @image = Mercury::Image.find(params[:id])
+#    @image.destroy
+#    respond_with @image
+#  end
 
-end
+#end
