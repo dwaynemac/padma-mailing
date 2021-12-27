d6e751c3ff4fe81f4372d9f88a2ddf25ac7db9aa
Dwayne Macgowan <dwaynemac@gmail.com>
2021-12-27 10:47:31 -0300

    update padma_crm_api.rb

--- lib/padma_crm_api.rb
+++ lib/padma_crm_api.rb
@@ -1,6 +1,14 @@
 class PadmaCrmApi
 
-  def self.update_contact(padma_id, attributes = {}, options = {})
+  attr_accessor :last_response
+
+  def initialize(options = {})
+  end
+
+  # ENV["crm_contacts_v0_api_key"]
+  # ENV["crm_v0_api_key"]
+
+  def update_contact(padma_id, attributes = {}, options = {})
     params = {
       app_key: ENV["crm_contacts_v0_api_key"]
     }
@@ -16,11 +24,63 @@ class PadmaCrmApi
     end
   end
 
-  def self.crm_url
+  def crm_url
     if Rails.env.development?
       "http://localhost:3000"
     else
       "https://crm.padm.am"
+    end
+  end
+
+  # @param attributes
+  # @option username
+  # @option account_name
+  # @option observations
+  # @option commented_at
+  # @option contact_id
+  #
+  # @example
+  #   api = PadmaCrmApi.new
+  #   api.create_comment(contact_id: "52f2c45e598abe77c70000ec", account_name: "cervino", username: "system", observations: "test", commented_at: Time.zone.now)
+  #
+  # @return Boolean
+  def create_comment(attributes)
+    response = Typhoeus.post("#{crm_url}/api/v0/comments",
+      body: {
+        comment: attributes,
+      },
+      params: {app_key: ENV["crm_v0_api_key"]},
+    )
+    self.last_response = response
+    response.code == 201
+  end
+
+  def update_comment(id, new_attributes)
+    res = Typhoeus.put("#{crm_url}/api/v0/comments/#{id}",
+      params: {
+        comment: new_attributes,
+        app_key: ENV["crm_contacts_v0_api_key"]
+      }
+    )
+    self.last_response = res
+    res.code == 204
+  end
+
+  def destroy_comment(id)
+    res = Typhoeus.delete("#{crm_url}/api/v0/comments/#{id}",
+      params: {app_key: ENV["crm_contacts_v0_api_key"]}
+    )
+    self.last_response = res
+    res.code == 204
+  end
+
+  def paginate_comments(where = {})
+    res = Typhoeus.get("#{crm_url}/api/v0/comments",
+      params: {app_key: ENV["crm_contacts_v0_api_key"], where: where}
+    )
+    self.last_response = res
+    if res.code == 200
+      ActiveSupport::JSON.decode(res.body)["collection"]
     end
   end
 
